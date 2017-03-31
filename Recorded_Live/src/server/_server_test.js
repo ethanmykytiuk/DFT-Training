fe124d250da67201b31a60296ce39f4d879f7ba0
ethanmykytiuk <emykytiu@ualberta.ca>
2017-03-31 15:35:06 -0600

    added hello world server response

--- Recorded_Live/src/server/_server_test.js
+++ Recorded_Live/src/server/_server_test.js
@@ -4,15 +4,32 @@ var server = require("./server.js");
 var http = require("http");
 var localHostUrl = "http://localhost:8080";
 
-exports.testHttpServer = function(test) {
-    server.start();
+exports.tearDown = function(done) {
+    server.stop(function(){
+        done();
+    });  
     
-    http.get(localHostUrl, function(response){
-        
-    });
-    test.done();
 };
 
+exports.test_serverReturnsHelloWorld = function(test) {
+    server.start();
+    var request = http.get(localHostUrl);
+    request.on("response", function(response) {
+        var receivedData = false;
+        response.setEncoding("utf8");
+        test.equals(200, response.statusCode);
+        
+        response.on("data", function(chunk){
+            receivedData = true;
+            test.equals("Hello World!", chunk, "response text");
+        });
+        response.on("end", function(){
+            test.equals(receivedData, true, "should have received data here");
+            test.done(); 
+        });
+    });
+
+};
 /*
 exports.testNothing = function(test) {
     test.equals(3,server.number(), "Numbers are not equal!");
--- Recorded_Live/src/server/server.js
+++ Recorded_Live/src/server/server.js
@@ -1,25 +1,15 @@
 "use strict";
+var http = require("http");
+var server;
 
 exports.start = function() {
-    var http = require("http");
-
-    var server = http.createServer();
-
+    server = http.createServer();
     server.on("request", function(request, response) {
-        console.log("Received request");
-
-        var body = "<html><head><title>Node HTTP Spike</title></head>" +
-                "<body><p>This is a spike of Node's HTTP server.</p></body></html>";
-
-        // The following two approaches are equivalent:
-        // The verbose way...
-    //	response.statusCode = 200;
-    //	response.write(body);
-    //	response.end();
-
-        // The concise way...
-        response.end(body);
+        response.end("Hello World!");
     });
-
     server.listen(8080);
+};
+
+exports.stop = function(callback) {
+  server.close(callback);  
 };
