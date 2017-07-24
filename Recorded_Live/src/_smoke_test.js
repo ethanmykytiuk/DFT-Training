6921eb320545822dea3eab460fe8d00f99594592
ethanmykytiuk <emykytiu@ualberta.ca>
2017-07-24 15:02:59 -0600

    adding smoke test for wiki paint home page (Episode 26)

--- Recorded_Live/src/_smoke_test.js
+++ Recorded_Live/src/_smoke_test.js
@@ -6,7 +6,8 @@
     "use strict";
     var http = require("http");
 	var child_process = require("child_process");
-
+    var child;
+    
     var TEST_HOME_PAGE = "generated/test/testHome.html";
     var TEST_404_PAGE = "generated/test/test404.html";
     
@@ -30,30 +31,33 @@
    
     
 	function runServer(callback) {
-		var child = child_process.spawn("node", ["src/server/weewikipaint", "8080"]);
+		child = child_process.spawn("node", ["src/server/weewikipaint", "8080"]);
 		child.stdout.setEncoding("utf8");
 		child.stdout.on("data", function(chunk) {
 			process.stdout.write("server stdout: " + chunk);
 			if (chunk.trim() === "Server started") callback();
-		});
-		child.stderr.on("data", function(chunk) {
-			process.stdout.write("server stderr: " + chunk);
-		});
-		child.on("exit", function(code, signal) {
-			console.log("Server process exited with code [" + code + "] and signal [" + signal + "]");
 		});
 	}
     
     
     /******** TESTS ********/
 
-	exports.test_for_smoke = function(test) {
-		runServer(function(){
-			console.log("callback called");
-            httpGet("http://localhost:8080", function(response, receivedData) {
-				console.log("got file");
-				test.done();
-			});
+    exports.setUp = function(done) {
+        runServer(done);
+    };
+    
+    exports.tearDown = function(done){
+        child.on("exit", function(code, signal){
+            done();
+        });
+        child.kill();
+    };
+    
+	exports.test_canGetHomePage = function(test) {
+        httpGet("http://localhost:8080", function(response, receivedData) {
+            var foundHomePage = receivedData.indexOf("WeeWikiPaint home page") !== -1;
+            test.ok(foundHomePage, "home page should have contained WeeWikiPaint marker");
+            test.done();
         });
 	};
     
--- Recorded_Live/src/server/weewikipaint.js
+++ Recorded_Live/src/server/weewikipaint.js
@@ -1,7 +1,7 @@
 (function() { 
     "use strict";
-    var HOME_PAGE = "src/server/homepage.html";
-    var ERROR_PAGE = "src/server/404.html";
+    var HOME_PAGE = "src/server/content/homepage.html";
+    var ERROR_PAGE = "src/server/content/404.html";
     
     var server = require("./server.js");
     server.start(HOME_PAGE, ERROR_PAGE, 8080, function(){
