f8d901f002bfb411cab3d5c85cdfd983282c15b5
ethanmykytiuk <emykytiu@ualberta.ca>
2017-08-25 09:30:49 -0600

    separate production code from test code and updated karma to look at whole client folder

--- Recorded_Live/build/karma.conf.js
+++ Recorded_Live/build/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'src/client/_*.js'
+        'src/client/*.js'
     ],
 
 
---
+++ Recorded_Live/src/client/client.js
@@ -0,0 +1,16 @@
+/*global dump*/
+
+(function() {
+    "use strict";
+    
+    
+    window.onload = function(){
+        var div = document.createElement("div");
+        div.setAttribute("id", "tdjs");
+        div.setAttribute("foo", "bar");
+        document.body.appendChild(div);  
+        
+        dump("Window loaded");
+    };
+    
+}());
