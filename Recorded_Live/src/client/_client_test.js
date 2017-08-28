7ca8558e9b3ebc211f5e921d584d5787ff6890c0
ethanmykytiuk <emykytiu@ualberta.ca>
2017-08-28 16:49:56 -0600

    cross browser test for Raphael

--- Recorded_Live/src/client/_client_test.js
+++ Recorded_Live/src/client/_client_test.js
@@ -10,6 +10,7 @@
     describe("Drawing area", function() {
 
         it("should be initialized in predefined div", function() {
+            
             // create div (assumed to be on homepage)
             var div = document.createElement("div");
             div.setAttribute("id", "wwp-drawingArea");
@@ -19,9 +20,17 @@
             wwp.initializeDrawingArea("wwp-drawingArea");
             
             //verify it was initialized properly
-            var tagName = $(div).children()[0].tagName;
-            expect(tagName).to.equal("svg");
-            dump($(div).children()[0].tagName);
+            var tagName = $(div).children()[0].tagName.toLowerCase();
+                        
+            if(tagName === "svg"){
+                // In browser that supports SVG
+                expect(tagName).to.equal("svg");        
+            }
+            else
+            {
+                // In browser that doesn't support SVG (IE 8)
+                expect(tagName).to.equal("div");  
+            }
         });
 
     });
