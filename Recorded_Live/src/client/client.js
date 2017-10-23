581192959a48a320a5ab755fd94e4b21dfa0a9f3
ethanmykytiuk <emykytiu@ualberta.ca>
2017-10-23 16:46:36 -0600

    QUick spike to draw line from 0,0 to where the mouse is clicked

--- Recorded_Live/src/client/client.js
+++ Recorded_Live/src/client/client.js
@@ -1,4 +1,4 @@
-/*global dump, Raphael, wwp:true*/
+/*global dump, $, Raphael, wwp:true*/
 
 wwp = {};
 
@@ -8,7 +8,18 @@ wwp = {};
     var paper;
     
     wwp.initializeDrawingArea = function(drawingAreaElement){
-        paper = new Raphael(drawingAreaElement);        
+        paper = new Raphael(drawingAreaElement);
+        $(drawingAreaElement).click(function(event){
+            
+            var divPageX = $(drawingAreaElement).offset().left;
+            var divPageY = $(drawingAreaElement).offset().top;
+            var relativeX = event.pageX - divPageX;
+            var relativeY = event.pageY - divPageY;
+            
+            
+            wwp.drawLine(0,0,relativeX, relativeY);
+        });
+        
         return paper;
     };
     
