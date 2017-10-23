31f47e7a08cfd141b0b5037656d0f6baf035d806
ethanmykytiuk <emykytiu@ualberta.ca>
2017-10-23 17:02:29 -0600

    adding to spike to work with dragging the mouse

--- Recorded_Live/src/client/client.js
+++ Recorded_Live/src/client/client.js
@@ -8,16 +8,33 @@ wwp = {};
     var paper;
     
     wwp.initializeDrawingArea = function(drawingAreaElement){
+        var prevX = null;
+        var prevY = null;
+        
+        var isDragging = false;
+        var jqArea = $(drawingAreaElement);
+        
+        //TODO in test: if mouse clicked when outside of element, or let go outside of element
+        // isDragging state may get stuck
+        jqArea.mousedown(function(event){
+           isDragging = true; 
+        });
+        
+        jqArea.mouseup(function(event){
+           isDragging = false; 
+        });
+        
         paper = new Raphael(drawingAreaElement);
-        $(drawingAreaElement).click(function(event){
+        $(drawingAreaElement).mousemove(function(event){
             
-            var divPageX = $(drawingAreaElement).offset().left;
-            var divPageY = $(drawingAreaElement).offset().top;
+            var divPageX = jqArea.offset().left;
+            var divPageY = jqArea.offset().top;
             var relativeX = event.pageX - divPageX;
             var relativeY = event.pageY - divPageY;
             
-            
-            wwp.drawLine(0,0,relativeX, relativeY);
+            if(prevX !== null && isDragging) wwp.drawLine(prevX, prevY, relativeX, relativeY);
+            prevX = relativeX;
+            prevY = relativeY;
         });
         
         return paper;
