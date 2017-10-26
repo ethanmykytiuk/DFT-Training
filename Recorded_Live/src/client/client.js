9bdb7619f77d70d474b4b6ceb8f8cea7617bd8f5
ethanmykytiuk <emykytiu@ualberta.ca>
2017-10-26 15:36:46 -0600

    Cleaning up production code

--- Recorded_Live/src/client/client.js
+++ Recorded_Live/src/client/client.js
@@ -16,35 +16,37 @@ wwp = {};
         };
     }
     
-    wwp.initializeDrawingArea = function(drawingAreaElement){
+    function handleDragEvents(drawingAreaElement){
         var start = null;
         var drawingArea = $(drawingAreaElement);      
 
-        paper = new Raphael(drawingAreaElement);
-        
         $(document).mousedown(function(event) {
             start = relativeOffset(drawingArea, event.pageX, event.pageY);
         });
-        
-        $(document).mouseup(function(event) {
-            start = null;
-        });
-        
+                       
         drawingArea.mousemove(function(event){
-            
             if(start === null) return;
-
+            
             var end = relativeOffset(drawingArea, event.pageX, event.pageY);
             
             if(start !== null) wwp.drawLine(start.x, start.y, end.x, end.y);
             start = end;
-        });  
-       
+        }); 
+        
+        $(document).mouseup(function(event) {
+            start = null;
+        });
+ 
+    }
+    
+    wwp.initializeDrawingArea = function(drawingAreaElement){
+        handleDragEvents(drawingAreaElement);
+        paper = new Raphael(drawingAreaElement);
         return paper;
     };
     
     wwp.drawLine = function(startX, startY, endX, endY) {
 			paper.path("M" + startX + "," + startY + "L" + endX + "," + endY);
-		};
+    };
 	
 }());
