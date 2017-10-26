a557b7b3082ae2de7db24dd2701ad5e625b2be5b
ethanmykytiuk <emykytiu@ualberta.ca>
2017-10-26 12:08:13 -0600

    Removing commented out spike and getting rid of dragging flag

--- Recorded_Live/src/client/client.js
+++ Recorded_Live/src/client/client.js
@@ -18,58 +18,28 @@ wwp = {};
     
     wwp.initializeDrawingArea = function(drawingAreaElement){
         var start = null;
-        var isDragging = false;
         var drawingArea = $(drawingAreaElement);      
 
         paper = new Raphael(drawingAreaElement);
         
         $(document).mousedown(function(event) {
-            isDragging = true;
             start = relativeOffset(drawingArea, event.pageX, event.pageY);
         });
         
         $(document).mouseup(function(event) {
-            isDragging = false;
+            start = null;
         });
         
         drawingArea.mousemove(function(event){
-            var pageOffset = drawingArea.offset();
+            
+            if(start === null) return;
+
             var end = relativeOffset(drawingArea, event.pageX, event.pageY);
             
-            if(start !== null && isDragging) wwp.drawLine(start.x, start.y, end.x, end.y);
+            if(start !== null) wwp.drawLine(start.x, start.y, end.x, end.y);
             start = end;
-        });
-        
-        /*
-        var prevX = null;
-        var prevY = null;
-        
-        var isDragging = false;
-        var jqArea = $(drawingAreaElement);
-        
-        //TODO in test: if mouse clicked when outside of element, or let go outside of element
-        // isDragging state may get stuck
-        jqArea.mousedown(function(event){
-           isDragging = true; 
-        });
-        
-        jqArea.mouseup(function(event){
-           isDragging = false; 
-        });
-        
-        $(drawingAreaElement).mousemove(function(event){
-            
-            var divPageX = jqArea.offset().left;
-            var divPageY = jqArea.offset().top;
-            var relativeX = event.pageX - divPageX;
-            var relativeY = event.pageY - divPageY;
-            
-            if(prevX !== null && isDragging) wwp.drawLine(prevX, prevY, relativeX, relativeY);
-            prevX = relativeX;
-            prevY = relativeY;
-        });
-        */
-        
+        });  
+       
         return paper;
     };
     
